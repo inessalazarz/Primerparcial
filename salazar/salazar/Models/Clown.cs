6bc817ef1a2fffc1b9f6bbb18a222632fc17f3aa
shijominagashiro@gmail.com <shijominagashiro@gmail.com>
2019-09-28 12:12:36 -0400

    Se creó el modelo, el controlador y las vistas

---
+++ salazar/salazar/Models/Clown.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace salazar.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum trickType
+    {
+        imagen,
+        icono,
+        incendio
+
+    }
+    public class Clown
+    {
+        [Key]
+        public int ClownId { get; set; }
+        [Required]
+        [StringLength(24)]
+        public string NickName { get; set; }
+        
+        [Required]
+        public trickType Tricks { get; set; }
+        public string Email { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
---
+++ salazar/salazar/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace salazar.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<salazar.Models.Clown> Clowns { get; set; }
+    }
+}
